7846b87384ebbc24ecb0b5af1710a85138e1ccdc
Scott Shawcroft <scott@tannewt.org>
2024-06-04 21:14:51 -0700

    Start send

--- replay.py
+++ replay.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 import json
@@ -7,6 +8,29 @@ print(input_file)
 
 language_server = subprocess.Popen(sys.argv[2:], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
+def send(method, params, request_id=None):
+	rpc = {"jsonrpc": "2.0", "method": method, "params": params}
+	if request_id is not None:
+		rpc["id"] = request_id
+	payload = json.dumps(rpc).encode("utf-8")
+	language_server.stdin.write(f"Content-Length: {len(payload)}\r\n".encode("ascii"))
+	language_server.stdin.write(b"\r\n")
+	language_server.stdin.write(payload)
+	language_server.stdin.flush()
+
+def receive():
+	content_length = 0
+	while True:
+		line = language_server.stdout.readline().decode("ascii").strip()
+		if line == "":
+			break
+		if line.startswith("Content-Length: "):
+			content_length = int(line[len("Content-Length: "):])
+	return json.loads(language_server.stdout.read(content_length))
+
+send("initialize", {"processId": os.getpid(), "clientInfo": {"name": "LSP Replay"}, "capabilities": {"textDocument": {}}}, 1)
+print(receive())
+
 with open(input_file, "r") as f:
 	for line in f:
 		event = json.loads(line)
@@ -14,4 +38,12 @@ with open(input_file, "r") as f:
 			continue
 		params = event["params"]
 		del event["params"]
-		print(event)
+		#print(event)
+
+print("sending shutdown")
+send("shutdown", {})
+print(receive())
+
+print("sending exit")
+send("exit", {})
+language_server.wait()
