89eb223265bab77c2cdd11f2cc3c9870cf19820d
Scott Shawcroft <scott@tannewt.org>
2024-06-07 11:03:19 -0700

    more work

--- replay.py
+++ replay.py
@@ -6,7 +6,8 @@ import json
 input_file = sys.argv[1]
 print(input_file)
 
-language_server = subprocess.Popen(sys.argv[2:], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+LLM_LOG_LEVEL="DEBUG"
+language_server = subprocess.Popen(sys.argv[2:], stdin=subprocess.PIPE, stdout=subprocess.PIPE, env={"LLM_LOG_LEVEL": LLM_LOG_LEVEL})
 
 def send(method, params, request_id=None):
 	rpc = {"jsonrpc": "2.0", "method": method, "params": params}
@@ -24,6 +25,7 @@ def receive():
 		line = language_server.stdout.readline().decode("ascii").strip()
 		if line == "":
 			break
+		print(line)
 		if line.startswith("Content-Length: "):
 			content_length = int(line[len("Content-Length: "):])
 	return json.loads(language_server.stdout.read(content_length))
@@ -31,19 +33,52 @@ def receive():
 send("initialize", {"processId": os.getpid(), "clientInfo": {"name": "LSP Replay"}, "capabilities": {"textDocument": {}}}, 1)
 print(receive())
 
+open_files = set()
+
 with open(input_file, "r") as f:
+	i = 0
 	for line in f:
 		event = json.loads(line)
 		if "copilot" not in event["server"] or event["direction"] == 2:
 			continue
 		params = event["params"]
-		del event["params"]
-		#print(event)
+		if event["method"].startswith("textDocument/"):
+			if "lsp-log" in event["params"]["textDocument"]["uri"]:
+				continue
+			uri = event["params"]["textDocument"]["uri"]
+			if event["method"] == "textDocument/didOpen":
+				open_files.add(uri)
+				send(event["method"], params)
+				print("opened", uri)
+			elif event["method"] == "textDocument/didClose":
+				if uri in open_files:
+					open_files.remove(uri)
+					send(event["method"], params)
+					print("closed", uri)
+			elif event["method"] == "textDocument/didChange":
+				if uri in open_files:
+					send(event["method"], params)
+					print(event)
+					print("changed", uri)
+				print(event)
+			else:
+				print(i, event)
+		else:
+			uri = params["doc"]["uri"]
+			if "lsp-log" in uri:
+				continue
+			if event["method"] == "getCompletionsCycling":
+				continue
+			del event["params"]["doc"]["source"]
+			print(i, event)
+		i += 1
+		if i > 10:
+			break
 
 print("sending shutdown")
 send("shutdown", {})
-print(receive())
+# print(receive())
 
-print("sending exit")
-send("exit", {})
-language_server.wait()
+# print("sending exit")
+# send("exit", {})
+# language_server.wait()
